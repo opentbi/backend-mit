678506cd3a8aea8e8255e3052732e63ca66e77ae
Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>
2023-03-12 04:23:39 +0000

    feat: init bot
    Signed-off-by: Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>

--- src/config.rs
+++ src/config.rs
@@ -2,9 +2,9 @@ use std::fs;
 
 use serde::{Serialize, Deserialize};
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Config {
-    pub app_id: u64,
+    pub app_id: i32,
     pub app_hash: String,
     pub telegram_bot_token: String,
     pub telegram_channel: String
@@ -18,7 +18,6 @@ impl Config {
             .expect("Isi file config dapat di parse oleh serde_json");
 
         drop(contents);
-
         deserialized
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,47 @@
 extern crate serde_json;
+extern crate grammers_client;
 
 mod config;
 
-use crate::config::Config;
+use grammers_client::{Client, Config};
+use grammers_session::Session;
+use tokio::runtime;
+use crate::config::Config as MitConfig;
 
-fn main() {
-    let cfg = Config::load();
+type Result = std::result::Result<(), Box<dyn std::error::Error>>;
 
-    println!("{:?}", cfg);
+fn takes_hash_own(hash: String) -> String {
+    let mut s1 = String::new();
+    s1.clone_from(&hash);
+
+    s1
+}
+
+async fn mit_main(cfg: MitConfig) -> Result {
+    let app_hash = takes_hash_own(cfg.app_hash);
+    let client =  Client::connect(Config {
+        api_id: cfg.app_id,
+        api_hash: app_hash.to_string(),
+        session: Session::load_file_or_create("malingit")?,
+        params: Default::default()
+    }).await?;
+
+    if !client.is_authorized().await? {
+        client.bot_sign_in(&cfg.telegram_bot_token, cfg.app_id, &app_hash).await?;
+        client.session().save_to_file("malingit")?;
+
+        println!("Logged in as: {}", client.get_me().await?.username().unwrap());
+    }
+
+    drop(app_hash);
+    Ok({})
+}
+
+fn main() -> Result {
+    let cfg = MitConfig::load();
+    runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .unwrap()
+        .block_on(mit_main(cfg))
 }
