e029548b32b68e1ed9e0f3372d18d40f584dfd22
Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>
2023-04-09 10:46:53 +0000

    feat(transferdata.rs): add WebDownloadFile
    Signed-off-by: Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>

--- src/transferdata.rs
+++ src/transferdata.rs
@@ -7,10 +7,21 @@ pub struct WebSearchFileData {
     pub file_size: i64,
     pub file_mime: String
 }
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct WebDownloadFile {
+    pub file_id: i64,
+    pub name: String
+}
+
 #[derive(Debug)]
 pub enum TransferData {
     WebSearchFile {
         query: String,
         resp_tx: tokio::sync::oneshot::Sender<Option<Vec<WebSearchFileData>>>
+    },
+    WebDownloadFile {
+        file_id: i64,
+        resp_tx: tokio::sync::oneshot::Sender<Option<WebDownloadFile>>
     }
 }
