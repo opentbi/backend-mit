9a2a46c6c4e906d0c77e739523ba565b76ea1276
Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>
2023-04-09 10:41:13 +0000

    feat(rest_util.rs): add is_json on ResultRestFn
    Added is_json on ResultRestFn to identify the response is json or text
    
    Signed-off-by: Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>

--- src/rest_util.rs
+++ src/rest_util.rs
@@ -4,5 +4,6 @@ use hyper::{StatusCode, body::Bytes, Response};
 pub struct ResultRestFn {
     pub status: StatusCode,
     pub msg: Full<Bytes>,
+    pub is_json: bool
 }
 pub type ResultServiceFn = Result<Response<Full<Bytes>>, hyper::Error>;
